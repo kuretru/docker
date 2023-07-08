a99fd695b49996e1db389d4b9e3ce4ce39bc1f60
呉真 <kuretru@gmail.com>
2023-07-08 17:15:25 +0800

    Change: move manager-address to entrypoint

--- shadowsocks-libev_with-v2ray-plugin/entrypoint.sh
+++ shadowsocks-libev_with-v2ray-plugin/entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-# vim:sw=4:ts=4:et
 
-ss-manager -c /config/config.json -u -D /var/run/ss-manager &
+ss-manager -c /config/config.json -u --manager-address 127.0.0.1:6001 -D /var/run/ss-manager &
 
-exec $@
+exec "$@"
